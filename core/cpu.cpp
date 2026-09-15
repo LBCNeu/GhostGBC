14d007c32f30ee0607721e62109474c809a7ffb8
lbcn.eu <lb.contentnetwork@gmail.com>
2026-09-15 19:04:14 +0200

    More Opcode Variants

--- core/cpu.cpp
+++ core/cpu.cpp
@@ -27,9 +27,12 @@ void cpu::step(std::array<uint8_t, 65536> &memory) // using std::array instead o
 
     switch (opcode)
     {
+
     case 0x00: // NOP... Nothing... Absolutely Nothing
         lastCycles = 4;
         break;
+
+        // ------------- LD Reg, u8 ----------------
 
     case 0x06: // LD B, u8... Load unsigned 8-bit int into B-Reg
         B = memory[PC];
@@ -37,40 +40,156 @@ void cpu::step(std::array<uint8_t, 65536> &memory) // using std::array instead o
         lastCycles = 8;
         break;
 
-    case 0x0E: // LD C, u8... Load unsigned 8-bit int into C-Reg
+    case 0x0E:
         C = memory[PC];
         PC++;
         lastCycles = 8;
         break;
 
-    case 0x16: // LD D, u8... Load unsigned 8-bit int into D-Reg
+    case 0x16:
         D = memory[PC];
         PC++;
         lastCycles = 8;
         break;
 
-    case 0x1E: // LD E, u8... Load unsigned 8-bit int into E-Reg
+    case 0x1E:
         E = memory[PC];
         PC++;
         lastCycles = 8;
         break;
 
-    case 0x26: // LD H, u8... Load unsigned 8-bit int into H-Reg
+    case 0x26:
         H = memory[PC];
         PC++;
         lastCycles = 8;
         break;
 
-    case 0x2E: // LD L, u8... Load unsigned 8-bit int into L-Reg
+    case 0x2E:
         L = memory[PC];
         PC++;
         lastCycles = 8;
         break;
 
-    case 0x3E: // LD A, u8... Load unsigned 8-bit int into A-Reg
-        D = memory[PC];
+    case 0x3E:
+        A = memory[PC];
         PC++;
         lastCycles = 8;
+        break;
+
+        // ------------- LD Reg, u16 ----------------
+
+    case 0xFA:
+    { // LD A, u16
+        // TODO
+        uint16_t addr = memory[PC];
+        PC++;
+        addr = addr | (memory[PC] << 8);
+        A = memory[addr];
+        PC++;
+        lastCycles = 16;
+        break;
+    }
+        // ------------- LD Reg -> Reg ----------------
+
+    case 0x78: // LD A, B... Load unsigned B-Reg data int into A-Reg
+        A = B;
+        lastCycles = 4;
+        break;
+
+    case 0x79:
+        A = C;
+        lastCycles = 4;
+        break;
+
+    case 0x7A:
+        A = D;
+        lastCycles = 4;
+        break;
+
+    case 0x7B:
+        A = E;
+        lastCycles = 4;
+        break;
+
+    case 0x7C:
+        A = H;
+        lastCycles = 4;
+        break;
+
+    case 0x7D:
+        A = L;
+        lastCycles = 4;
+        break;
+
+    case 0x7E: // LD A, (HL) <- Brackets in the name mean that is has to load the Value into A and not the adress
+        A = memory[getHL()];
+        lastCycles = 8;
+        break;
+
+    case 0x7F:
+        A = A;
+        lastCycles = 4;
+        break;
+
+    case 0x0A:
+        A = memory[getBC()];
+        lastCycles = 8;
+        break;
+
+    case 0x1A:
+        A = memory[getDE()];
+        lastCycles = 8;
+        break;
+
+    case 0x2A: // LD A, (HL+) HL gets incremented after Operation
+        A = memory[getHL()];
+        setHL(getHL() + 1); // Adress in HL Reg gets inremented by 1 so it points to the next Byte
+        lastCycles = 8;
+        break;
+
+    case 0x3A: // LD A, (HL-) HL gets decremented after Operation
+        A = memory[getHL()];
+        setHL(getHL() - 1); // Adress in HL Reg gets decremented by 1 so it points to the Byte before
+        lastCycles = 8;
+        break;
+
+    case 0xF0: // LD A, (FF00+u8) Load Value in Adress FF00+u8 into A Reg
+    {
+        uint16_t addr = 0xFF00 + memory[PC]; // Adress is composed of 0xFF00 and the Byte next to the Opcode
+        A = memory[addr];
+        PC++;
+        lastCycles = 8;
+        break;
+    }
+
+    case 0xF2: // LD A, (FF00+C) Load Value in Adress FF00 + C into A Reg
+    {
+        uint16_t addr = 0xFF00 + C; // Adress is composed of 0xFF00 and C Reg and no following Byte -> no PC++ needed
+        A = memory[addr];
+        lastCycles = 8;
+        break;
+    }
+
+        // ------------- INC ----------------
+
+    case 0x3C: // INC A Increase Value of A-Reg by 1
+        A++;
+        setZeroFlag(A == 0); // Only set ZeroFlag if Result is Zero
+        setSubFlag(false);
+        setHalfFlag((A & 0x0F) == 0x00); // Half Carry checks i there was a carry from the lower 4 bits to the higher. A gets put through a mask (0x0F, 1111) with & to check the low 4 bits, then it gets checked if they are all Zero, that would mean that a carry took place
+        // INC does not touch the Carry Flag!!!
+        lastCycles = 4;
+        break;
+
+        // ------------- DEC ----------------
+
+    case 0x3D: // DEC A Decrease Value of A-Reg by 1
+        A--;
+        setZeroFlag(A == 0); // Only set ZeroFlag if Result is Zero
+        setSubFlag(true);
+        setHalfFlag((A & 0x0F) == 0x0F); // Half Borrow checks i there was a "reverse" carry from the higher 4 bits to the lower. A gets put through a mask (0x0F, 1111) with & to check the low 4 bits, then it gets checked if they are all 1, that would mean that a "reverse" carry/borrow took place
+        // DEC does not touch the Carry Flag!!!
+        lastCycles = 4;
         break;
 
     default:
